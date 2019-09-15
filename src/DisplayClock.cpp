26431cb6e92e48f059faadda779b257f2983a31e
Mike Moik <mikemoik@gmail.com>
2019-09-15 11:25:10 +0200

    .

--- src/DisplayClock.cpp
+++ src/DisplayClock.cpp
@@ -6,8 +6,46 @@ Display::Clock::Clock()
   return;
 }
 
-void Display::Clock::blink(void)
+void Display::Clock::setTime(uint8_t h, uint8_t m)
 {
+  _h = h;
+  _m = m;
+  _s = 0xFFU;
 
   return;
+}
+void Display::Clock::setTime(uint8_t h, uint8_t m, uint8_t s)
+{
+  _h = h;
+  _m = m;
+  _s = s;
+
+  return;
+}
+
+void Display::Clock::setStatus(String status)
+{
+  _status = status;
+
+  return;
+}
+
+void Display::Clock::setDebug(String debug)
+{
+  _debug = debug;
+
+  return;
+}
+
+uint8_t Display::Clock::getHours(void)
+{
+  return _h;
+}
+uint8_t Display::Clock::getMinutes(void)
+{
+  return _m;
+}
+uint8_t Display::Clock::getSeconds(void)
+{
+  return _s;
 }
--- src/DisplayClock.h
+++ src/DisplayClock.h
@@ -12,11 +12,30 @@ namespace Display
     Clock();
     //˜Clock(){}
 
-    void blink(void);
+    void begin() = 0;
+
+    void setTime(uint8_t h, uint8_t m, uint8_t s);
+    void setTime(uint8_t h, uint8_t m);
+    void setStatus(String status);
+    void setDebug(String debug);
+
+    uint8_t getHours(void);
+    uint8_t getMinutes(void);
+    uint8_t getSeconds(void);
+
+    void showTime() = 0;
+    void showStatus() = 0;
+    void showDebug() = 0;
+    void refresh() = 0;
 
   private:
     Clock(Clock const&)     = delete;
     void operator=(Clock const&) = delete;
+
+    uint8_t _h,_m,_s;
+
+    String _status;
+    String _debug;
   };
 
 };
