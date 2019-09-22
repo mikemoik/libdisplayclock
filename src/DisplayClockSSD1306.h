d49fc1d736329658d0c5185d01aa0f3d45430c16
Mike Moik <mikemoik@gmail.com>
2019-09-22 10:16:35 +0200

    .

--- src/DisplayClockSSD1306.h
+++ src/DisplayClockSSD1306.h
@@ -3,6 +3,16 @@
 
 #include <DisplayClock.h>
 
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define CHAR_PIXEL 6 // x and y pixels of a char
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     LED_BUILTIN // Reset pin # (or -1 if sharing Arduino reset pin)
+
 namespace Display
 {
   class ClockSSD1306
